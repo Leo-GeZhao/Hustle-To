ef00555c8c97d7894ec59fea1e33d332c8f52508
Ge Zhao <gezhao@Ges-MacBook-Pro.local>
2022-11-27 19:22:24 -0500

    add sneaker usestate in detail

--- src/pages/AdminPage/AdminProductDetailPage.jsx
+++ src/pages/AdminPage/AdminProductDetailPage.jsx
@@ -1,21 +1,27 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+
 import * as adminAPI from '../../utilities/api/sneakers'
-export default function AdminProductDetailPage({sneaker,setSneakers}){
+export default function AdminProductDetailPage(){
+    
+    const [sneaker,setSneaker] = useState('')
+    
     const {sneakerName} = useParams()
+    
 
     useEffect(function(){
         async function getSneaker(sneakerName){
           const sneaker = await adminAPI.getSneaker(sneakerName);
           console.log(sneaker)
-          setSneakers(sneaker[0])
+          setSneaker(sneaker[0])
         }
         getSneaker(sneakerName);
       },[]);
+    
 
     return (
         <div className="card">
-            <div>Product Detail Page</div>
+            <h2>Product Detail Page</h2>
             <div>{sneaker.name}</div>
             <div>{sneaker.price}</div>
             <div>{sneaker.size}</div>
