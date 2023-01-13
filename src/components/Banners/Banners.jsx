ba7e3160837c3ed6ccd3ef72e69761e47e6e81dc
Ge Zhao <gezhao@ges-macbook-pro.home>
2023-01-13 01:02:34 -0500

    clear up banner

--- src/components/Banners/Banners.jsx
+++ src/components/Banners/Banners.jsx
@@ -1,28 +1,27 @@
-import * as adminAPI from '../../utilities/api/admin'
-import './Banners.css'
-export default function Banners({banner, setBanners}){
-    
-    async function handleDelete(evt){
-        evt.preventDefault()
-        const id = banner._id
-        console.log(id)
-        try {
-            await adminAPI.deleteBanner(id);
-            setBanners(...banner,'')
-            
-        }catch(err){
-            console.log(err)
-        }
-    }
-    return (
-            <div className='d-flex flex-row mb-3 justify-content-around align-items-center'>
-                <div>
-                    <img className="card-img-top" src={`${banner.image}`} alt="" />
-                </div>
-                <div className='d-flex flex-column mb-3 justify-content-center align-items-center'>
-                    {banner.name}
-                    <button onClick={handleDelete} className="btn btn-danger text-center m-3">Delete</button>
-                </div>     
-            </div>
-    )
-} 
+import * as adminAPI from "../../utilities/api/admin";
+import "./Banners.css";
+export default function Banners({ banner, setBanners, update, setUpdate }) {
+  async function handleDelete(e) {
+    e.preventDefault();
+    const data = { id: banner._id };
+    await adminAPI.deleteBanner(data);
+    setUpdate(true);
+  }
+
+  return (
+    <div className="d-flex flex-row mb-3 justify-content-around align-items-center">
+      <div>
+        <img className="card-img-top" src={`${banner.image}`} alt="" />
+      </div>
+      <div className="d-flex flex-column mb-3 justify-content-center align-items-center">
+        {banner.name}
+        <button
+          onClick={handleDelete}
+          className="btn btn-danger text-center m-3"
+        >
+          Delete
+        </button>
+      </div>
+    </div>
+  );
+}
--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -1,65 +1,109 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import { useState, useEffect } from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { useState, useEffect } from "react";
+import { Routes, Route } from "react-router-dom";
 
 //components
-import NavBar from '../../components/NavBar/NavBar';
-import Footer from '../../components/Footer/Footer';
-import LoginForm from '../../components/Account/LoginForm/LoginForm';
-import SignUpForm from '../../components/Account/SignUpForm/SignUpForm';
-import LandingPage from '../CustomerPage/LandingPage/LandingPage';
-import DetailPage from '../CustomerPage/DetailPage/DetailPage';
-import AdminPage from '../AdminPage/AdminPage';
-import AddInventoryPage from '../AdminPage/AddInventoryPage';
-import AdminProductDetailPage from '../AdminPage/AdminProductDetailPage';
-import Banner from '../AdminPage/Banner';
+import NavBar from "../../components/NavBar/NavBar";
+import Footer from "../../components/Footer/Footer";
+import LoginForm from "../../components/Account/LoginForm/LoginForm";
+import SignUpForm from "../../components/Account/SignUpForm/SignUpForm";
+import LandingPage from "../CustomerPage/LandingPage/LandingPage";
+import DetailPage from "../CustomerPage/DetailPage/DetailPage";
+import Inventory from "../AdminPage/Inventory";
+import AddInventory from "../AdminPage/AddInventory";
+import ProductDetail from "../AdminPage/ProductDetail";
+import Banner from "../AdminPage/Banner";
 
 //helper
-import { getUser } from '../../utilities/services/users';
-import * as adminAPI from '../../utilities/api/admin'
+import { getUser } from "../../utilities/services/users";
+import * as adminAPI from "../../utilities/api/admin";
 
-
-import './App.css';
-
+import "./App.css";
 
 export default function App() {
   const [user, setUser] = useState(getUser());
-  const [sneakers, setSneakers] = useState([])
-  const [banners,setBanners] = useState([])
+  const [sneakers, setSneakers] = useState([]);
+  const [banners, setBanners] = useState([]);
+  const [update, setUpdate] = useState(false);
 
-  useEffect(function(){
-    async function getSneakers(){
+  useEffect(function () {
+    async function getSneakers() {
       const allSneakers = await adminAPI.getSneakers();
-      setSneakers(...sneakers, allSneakers)
+      setSneakers(...sneakers, allSneakers);
     }
     getSneakers();
-  },[]);
+  }, []);
 
-  useEffect(function(){
-    async function getBanners(){
-      const allBanners = await adminAPI.getBanners();
-      setBanners(...banners,allBanners)
-    }
-    getBanners();
-  },[])
+  useEffect(
+    function () {
+      async function getBanners() {
+        const Banners = await adminAPI.getBanners();
+        setBanners(Banners.data);
+      }
+      getBanners();
+      setUpdate(false);
+    },
+    [update]
+  );
 
   return (
     <main className="App">
-            <NavBar user={user} setUser={setUser}/>
-            <div>
-              <Routes>
-                {/* Route components in here */}
-                <Route path="/login" element={<LoginForm setUser={setUser}/>} />
-                <Route path="/createaccount" element={<SignUpForm setUser={setUser}/>} />
-                <Route path="/admin/product" element={<AdminPage user={user} sneakers={sneakers} setSneakers={setSneakers}/>} />
-                <Route path="/admin/product/:sneakerName" element={<AdminProductDetailPage setSneakers={setSneakers}/>} />
-                <Route path="/admin/addinventory" element={<AddInventoryPage sneakers={sneakers} setSneakers={setSneakers}/>} />
-                <Route path="/admin/banner" element={<Banner user={user} banners={banners} setBanners={setBanners}/>} />
-                <Route path="/" element={<LandingPage sneakers={sneakers} setSneakers={setSneakers} banners={banners} setBanners={setBanners} />} />
-                <Route path="/product/:sneakerName" element={<DetailPage />} />
-              </Routes>
-            </div>
-            <Footer user={user} setUser={setUser}/>
+      <NavBar user={user} setUser={setUser} />
+      <div>
+        <Routes>
+          {/* Route components in here */}
+          <Route path="/login" element={<LoginForm setUser={setUser} />} />
+          <Route
+            path="/createaccount"
+            element={<SignUpForm setUser={setUser} />}
+          />
+          <Route
+            path="/admin/product"
+            element={
+              <Inventory
+                user={user}
+                sneakers={sneakers}
+                setSneakers={setSneakers}
+              />
+            }
+          />
+          <Route
+            path="/admin/product/:sneakerName"
+            element={<ProductDetail setSneakers={setSneakers} />}
+          />
+          <Route
+            path="/admin/addinventory"
+            element={
+              <AddInventory sneakers={sneakers} setSneakers={setSneakers} />
+            }
+          />
+          <Route
+            path="/admin/banner"
+            element={
+              <Banner
+                user={user}
+                banners={banners}
+                setBanners={setBanners}
+                update={update}
+                setUpdate={setUpdate}
+              />
+            }
+          />
+          <Route
+            path="/"
+            element={
+              <LandingPage
+                sneakers={sneakers}
+                setSneakers={setSneakers}
+                banners={banners}
+                setBanners={setBanners}
+              />
+            }
+          />
+          <Route path="/product/:sneakerName" element={<DetailPage />} />
+        </Routes>
+      </div>
+      <Footer user={user} setUser={setUser} />
     </main>
   );
 }
