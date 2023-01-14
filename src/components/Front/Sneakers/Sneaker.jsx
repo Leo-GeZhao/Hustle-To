c84661dd99bb8673abec64c86e27b8375f212b97
Ge Zhao <gezhao@ges-macbook-pro.home>
2023-01-14 05:13:28 -0500

    fix price showing

--- src/components/Front/Sneakers/Sneaker.jsx
+++ src/components/Front/Sneakers/Sneaker.jsx
@@ -2,12 +2,14 @@ import { Link } from "react-router-dom";
 import "./Sneakers.css";
 
 export default function Sneaker({ sneaker }) {
+  const lowestPrice = Math.min(...sneaker.variant.map((s) => s.price));
   return (
     <Link to={`/product/${sneaker.name}`}>
       <div className="card col">
         <img src={`${sneaker.image}`} alt="" />
         <div className="card-body">
           <h5 className="card-title">{sneaker.name}</h5>
+          <h5 className="card-title">From: {lowestPrice}.00 CAD</h5>
         </div>
       </div>
     </Link>
