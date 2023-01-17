59331832d026ea5e78f614b92fd6028f28a93c10
Ge Zhao <gezhao@ges-macbook-pro.home>
2023-01-17 03:01:56 -0500

    pull origin main

--- server.js
+++ server.js
@@ -3,21 +3,9 @@ const path = require("path");
 const favicon = require("serve-favicon");
 const logger = require("morgan");
 
-<<<<<<< HEAD
-// Always require and configure neat the top
-require("dotenv").config();
-// Connect to the database (after the dotenv)
-require("./config/database");
-require("./config/multer_S3");
-
-const userRouter = require("./routes/api/users");
-const sneakerRouter = require("./routes/api/sneakers");
-const bannerRouter = require("./routes/api/banners");
-=======
 require("dotenv").config();
 require("./config/database");
 require("./config/multer_S3");
->>>>>>> bbdf4167f0ac7ab5bfb4514904ef163493d82e21
 
 const app = express();
 
@@ -26,15 +14,6 @@ app.use(express.json());
 
 app.use(favicon(path.join(__dirname, "build", "favicon.ico")));
 app.use(express.static(path.join(__dirname, "build")));
-<<<<<<< HEAD
-
-app.use(require("./config/checkToken"));
-
-// API routes here
-app.use("/api/users", userRouter);
-app.use("/api/sneakers", sneakerRouter);
-app.use("/api/banners", bannerRouter);
-=======
 
 // API routes here
 app.use("/api/users", require("./routes/api/users"));
@@ -43,7 +22,6 @@ app.use("/api/banners", require("./routes/api/banners"));
 app.use("/api/inventories", require("./routes/api/inventories"));
 app.use("/api/carts", require("./routes/api/carts"));
 app.use("/api/checkouts", require("./routes/api/checkouts"));
->>>>>>> bbdf4167f0ac7ab5bfb4514904ef163493d82e21
 
 // "Catch all" route
 app.get("/*", function (req, res) {
